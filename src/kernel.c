377a02e84a3bc32bca779bf7637164c61b39df8c
Viliam Holly <viliamteoholly@gmail.com>
2026-03-25 20:00:52 +0100

    Improve formatting

--- src/kernel.c
+++ src/kernel.c
@@ -18,12 +18,13 @@ __attribute__((used,
     LIMINE_REQUESTS_END_MARKER;
 
 // Limine requests
-__attribute__((used, section(".limine_requests")))
-static volatile struct limine_memmap_request memmap_request = {
+__attribute__((
+    used,
+    section(".limine_requests"))) static volatile struct limine_memmap_request memmap_request = {
     .id = LIMINE_MEMMAP_REQUEST_ID, .revision = 0};
 
-__attribute__((used, section(".limine_requests")))
-static volatile struct limine_hhdm_request hhdm_request = {
+__attribute__((
+    used, section(".limine_requests"))) static volatile struct limine_hhdm_request hhdm_request = {
     .id = LIMINE_HHDM_REQUEST_ID, .revision = 0};
 
 // Halt and catch fire function
